7fa7dfeb0847ed550eff45d72886d3fdcb5ff4c2
Shad82103 <saw8210393@gmail.com>
2024-11-14 13:40:08 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SHARP
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7583040454:AAE5yMhRiPSSY3EVVLaVLlK68vhwljB7b7E")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("SHADOW")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6236477871")
+
+
+
+
+
+
+
+  
+ 
